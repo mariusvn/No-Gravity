cd71eeaf4c86edffd406b662f66c27b2c588d780
Marius Van Nieuwenhuyse <mariusvnspam@gmail.com>
2020-09-24 14:56:33 +0100

    tilemap switch

--- src/Tilemap.js
+++ src/Tilemap.js
@@ -2,13 +2,30 @@ import Tileset from "root/Tileset";
 import moonTileset from 'assets/tilesets/moon.png';
 import earthTileset from 'assets/tilesets/earth.png';
 import * as Collision from "root/Collision";
+import Game from "root/main";
 
 export default class Tilemap {
 
+  static TilesetType = {
+    EARTH: 'earth',
+    MOON: 'moon'
+  };
+
   container = new PIXI.Container();
-  tileset = new Tileset(earthTileset, {x: 32, y: 32});
+  containers = {
+    earth: new PIXI.Container(),
+    moon: new PIXI.Container()
+  };
+  tilesets = {
+    earth: new Tileset(earthTileset, {x: 32, y: 32}),
+    moon: new Tileset(moonTileset, {x: 32, y: 32})
+  };
+  currentTileset = Tilemap.TilesetType.EARTH;
   tilemap = [];
-  spriteList = [];
+  spriteList = {
+    earth: [],
+    moon: []
+  };
   width = 0;
   height = 0;
   tileRenderSize = 0;
@@ -33,6 +50,7 @@ export default class Tilemap {
     this.tileRenderSize = Math.round(heightpx / this.height);
     //this.dumpTilemap();
     this.generateTilemapContent();
+    Game.events.addEventHandler('gameplay:gravity-switch', this.switchTileset.bind(this));
   }
 
   dumpTilemap() {
@@ -53,7 +71,7 @@ export default class Tilemap {
       for (let x = colliderTilePos.x - 3; x < colliderTilePos.x + 5; x++) {
         if (x < 0 || y < 0 || x > this.width - 1 || y > this.height - 1)
           continue;
-        const sprite = this.spriteList[y][x];
+        const sprite = this.spriteList[this.currentTileset][y][x];
         if (sprite) {
           if (sprite.x > colliderBox.x + 200 || sprite.x < colliderBox.x - 200)
             continue;
@@ -106,23 +124,36 @@ export default class Tilemap {
     return this.tilemap[y][x];
   }
 
+  switchTileset(isOn) {
+    this.container.removeChildren();
+    if (isOn)
+      this.currentTileset = Tilemap.TilesetType.EARTH;
+    else
+      this.currentTileset = Tilemap.TilesetType.MOON;
+    this.container.addChild(this.containers[this.currentTileset]);
+  }
+
   generateTilemapContent() {
-    for (let y = 0; y < this.height; y++) {
-      this.spriteList[y] = [];
-      for (let x = 0; x < this.width; x++) {
-        if (this.tilemap[y][x] !== -1) {
-          const tile = this.tileset.getSprite(this.tilemap[y][x]);
-          tile.x = this.tileRenderSize * x;
-          tile.y = this.tileRenderSize * y;
-          tile.width = this.tileRenderSize;
-          tile.height = this.tileRenderSize;
-          this.container.addChild(tile);
-          this.spriteList[y].push(tile);
-        } else {
-          this.spriteList[y].push(undefined);
+    for (let tilesetType of Object.keys(Tilemap.TilesetType)) {
+      tilesetType = Tilemap.TilesetType[tilesetType];
+      for (let y = 0; y < this.height; y++) {
+        this.spriteList[tilesetType][y] = [];
+        for (let x = 0; x < this.width; x++) {
+          if (this.tilemap[y][x] !== -1) {
+            const tile = this.tilesets[tilesetType].getSprite(this.tilemap[y][x]);
+            tile.x = this.tileRenderSize * x;
+            tile.y = this.tileRenderSize * y;
+            tile.width = this.tileRenderSize;
+            tile.height = this.tileRenderSize;
+            this.containers[tilesetType].addChild(tile);
+            this.spriteList[tilesetType][y].push(tile);
+          } else {
+            this.spriteList[tilesetType][y].push(undefined);
+          }
         }
       }
     }
+    this.container.addChild(this.containers[this.currentTileset]);
   }
 
 }
