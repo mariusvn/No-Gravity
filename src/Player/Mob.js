b93f2b89f368583d177ebde772eab4e19c1c5589
Marius Van Nieuwenhuyse <mariusvnspam@gmail.com>
2020-09-24 13:02:03 +0100

    refacto mod update

--- src/Player/Mob.js
+++ src/Player/Mob.js
@@ -4,19 +4,19 @@ import player from 'assets/player/Player1.png';
 import * as Collision from "root/Collision";
 
 export default class Mob extends Entity {
-  Sprite;
+  sprite;
   tilemap;
   reverse = false;
   speed = 5;
 
   constructor(tilemap, x, y) {
     super(tilemap);
-    this.Sprite = new PIXI.Sprite(Game.app.loader.resources[player].texture);
-    const resizeRatio = (tilemap.tileRenderSize * 2) / (this.Sprite.height);
-    this.Sprite.scale.set(resizeRatio);
+    this.sprite = new PIXI.Sprite(Game.app.loader.resources[player].texture);
+    const resizeRatio = (tilemap.tileRenderSize * 2) / (this.sprite.height);
+    this.sprite.scale.set(resizeRatio);
     this.container.y = y;
     this.container.x = x;
-    this.container.addChild(this.Sprite);
+    this.container.addChild(this.sprite);
     this.tilemap = tilemap;
     this.setVelocity({x: 8, y: this.getVelocity().y});
   }
@@ -25,30 +25,32 @@ export default class Mob extends Entity {
     let playerPosition = player.getPosition();
     let mobPosition = this.getPosition();
     let playerBox = new PIXI.Rectangle(playerPosition.x, playerPosition.y, player.playerSprite.width, player.playerSprite.height);
-    let mobBox = new PIXI.Rectangle(mobPosition.x, mobPosition.y, this.Sprite.width, this.Sprite.height);
+    let mobBox = new PIXI.Rectangle(mobPosition.x, mobPosition.y, this.sprite.width, this.sprite.height);
     return !!Collision.hitTestRectangle(playerBox, mobBox);
 
   }
 
   move() {
-    let checkPos = this.getPosition();
-    checkPos.x = checkPos.x + this.getVelocity().x;
-    if (!this.reverse) {
-      checkPos.x = checkPos.x + this.getVelocity().x + this.Sprite.width;
-    }
-    checkPos.y += this.Sprite.height;
-    let tilecoord = this.tilemap.getTileCoord(checkPos);
-    if ((this.tilemap.getTile(tilecoord) === -1) || this.tilemap.getTile({
-      x: tilecoord.x,
-      y: tilecoord.y - 1
-    }) !== -1 || this.tilemap.getTile({x: tilecoord.x, y: tilecoord.y - 2}) !== -1) {
+    const userPos = Object.assign({}, this.getPosition());
+    const frontPositionX = (this.reverse) ? userPos.x + this.sprite.width : userPos.x;
+    const footPositionY = userPos.y + this.sprite.height;
+    const tilePos = this.tilemap.getTileCoord({x: frontPositionX, y: footPositionY - 1}); /* -1 to avoid between tile confusion */
+
+    if (this.tilemap.getTile({x: tilePos.x, y: tilePos.y + 1}) === -1) {
+      // If tile under the player is air
+      this.reverse = !this.reverse;
+    } else if (this.tilemap.getTile({x: tilePos.x + ((this.reverse) ? -1 : 1), y: tilePos.y + 1}) === -1) {
+      // If tile in front and under the player is air
+      this.reverse = !this.reverse;
+    } else if (this.tilemap.getTile({x: tilePos.x + ((this.reverse) ? -1 : 1), y: tilePos.y}) !== -1) {
+      // If tile in front of knees is filled
+      this.reverse = !this.reverse;
+    } else if (this.tilemap.getTile({x: tilePos.x + ((this.reverse) ? -1 : 1), y: tilePos.y -1}) !== -1) {
+      // If tile in front of head is filled
       this.reverse = !this.reverse;
     }
-    if (this.reverse) {
-      this.setVelocity({x: -this.speed, y: this.getVelocity().y});
-    } else
-      this.setVelocity({x: this.speed, y: this.getVelocity().y});
 
+    this.setVelocity({x: this.speed * ((this.reverse) ? -1 : 1), y: this.getVelocity().y});
   }
 
   update(delta, player) {
